ea79482e8f3ee2186a4b615d31ed3c53eaf803c0
Aleyna Yıldız <aleynayildizc@gmail.com>
2025-08-17 22:54:08 +0300

    add:baska cozum

--- whats_your_name.py
+++ whats_your_name.py
@@ -6,3 +6,16 @@ if __name__ == '__main__':
     first_name = input()
     last_name = input()
     print_full_name(first_name, last_name)
+
+""" şu şekilde de yazılabilir
+def print_full_name(first, last):
+    print("Hello " + first + " " + last + "! You just delved into python.")
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
+
+
+
+"""
