2b7a39cef7eb95bf70b7a8efa19420f8fb5b00db
Aleyna Yıldız <aleynayildizc@gmail.com>
2025-08-17 22:34:30 +0300

    string split and join

--- swapcase.py
+++ swapcase.py
@@ -4,3 +4,10 @@ if __name__ == '__main__':
     s = input()
     result = swap_case(s)
     print(result)
+
+#joinin farklı kullanımı
+"""
+words = ["slm","mrb"]
+result = " ".join(words)
+print(result)
+"""
